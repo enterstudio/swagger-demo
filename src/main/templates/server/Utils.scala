050a48bb4166a0723dc889843858d0c2eb0af235
Tony Tam <fehguy@gmail.com>
2012-12-04 23:37:43 -0800

    added 2nd example

---
+++ src/main/templates/server/Utils.scala
@@ -0,0 +1,25 @@
+package services
+
+import com.wordnik.util.perf.Profile
+
+case class ApiResponse(msg: String, code: Int)
+
+trait SwaggerDatatypeConversionSupport {
+	def IntDataType(obj: String): Int = obj.toInt
+
+	def IntDataType(obj: Option[String]): Option[Int] = obj match {
+		case Some(i) => Some(i.toInt)
+		case _ => None
+	}
+
+	def BooleanDataType(obj: String): Boolean = obj.toBoolean
+
+	def BooleanDataType(obj: Option[String]): Option[Boolean] = obj match {
+		case Some(i) => Some(i.toBoolean)
+		case _ => None
+	}
+
+	def StringDataType(obj: String): String = obj
+
+	def StringDataType(obj: Option[String]): Option[String] = obj
+}
